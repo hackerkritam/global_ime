be972b3462ad4b417bc7abdefa7be1d1deaffb46
Kritam Bhattarai <83293645+hackerkritam@users.noreply.github.com>
2025-05-04 23:41:42 +0545

    new-change

--- project/src/components/FraudDetection.tsx
+++ project/src/components/FraudDetection.tsx
@@ -33,7 +33,6 @@ function TransactionCard({
   const riskColors = {
     high: 'bg-red-100 text-red-800',
     medium: 'bg-yellow-100 text-yellow-800',
-    low: 'bg-green-100 text-green-800'
   }
 
   return (
