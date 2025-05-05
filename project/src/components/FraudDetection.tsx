075628054e470db1530588318e0a6f6d8f321b15
Kritam Bhattarai <83293645+hackerkritam@users.noreply.github.com>
2025-05-05 20:58:02 +0545

    4

--- project/src/components/FraudDetection.tsx
+++ project/src/components/FraudDetection.tsx
@@ -15,7 +15,19 @@ export default function FraudDetection() {
           icon={<Clock className="h-6 w-6" />}
           type="success"
         />
-           type="info"
+        <StatCard
+          title="Fraud Prevented"
+          value="₹2.5M"
+          change="+₹500K"
+          icon={<Shield className="h-6 w-6" />}
+          type="success"
+        />
+        <StatCard
+          title="Active Monitoring"
+          value="24/7"
+          change="100%"
+          icon={<CreditCard className="h-6 w-6" />}
+          type="info"
         />
       </div>
 
